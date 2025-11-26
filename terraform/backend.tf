40a3d796b30751147c6b023b46460b7c44825085
Javier Santiago Matallana Torres <javier.matallana+adlover@AvalDigitalLabs.com>
2025-11-25 23:48:31 -0500

    format terraform files

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "tfstate-ebook-app"
-    key = "ebook-app/production/terraform.tfstate"
-    region = "us-east-2"
+    bucket         = "tfstate-ebook-app"
+    key            = "ebook-app/production/terraform.tfstate"
+    region         = "us-east-2"
     dynamodb_table = "tfstate-ebook-app-lock"
-    encrypt = true
+    encrypt        = true
   }
 }
