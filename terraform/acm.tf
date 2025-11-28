a033a4e7335d797846ac9793be005dd4f54ef53c
Javier Santiago Matallana Torres <javier.matallana+adlover@AvalDigitalLabs.com>
2025-11-28 10:58:55 -0500

    deleting SAN

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -3,10 +3,6 @@ resource "aws_acm_certificate" "website_cloudfront_certificate" {
   domain_name       = local.domain_name
   validation_method = "DNS"
 
-  subject_alternative_names = [
-    "www.${local.domain_name}"
-  ]
-
   lifecycle {
     create_before_destroy = true
   }
